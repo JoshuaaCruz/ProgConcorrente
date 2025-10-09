4575ac80844cd11f10cbc0e5b8246e4078d2d4a3
Wa1Fuslayer <ricardao052meia@gmail.com>
2025-10-08 22:21:19 -0300

    Exercício 3 feito: proc. por confinamento de pilha

--- AF-mutex/AF-mutex/AF-mutex/exercicio_3/main.c
+++ AF-mutex/AF-mutex/AF-mutex/exercicio_3/main.c
@@ -3,34 +3,33 @@
 #include <stdlib.h>
 #include <assert.h>
 
-int gValue = 0;
-pthread_mutex_t gMtx;
-
 // Função imprime resultados na correção do exercício -- definida em helper.c
 void imprimir_resultados(int n, int** results);
 
 // Função escrita por um engenheiro
 int compute(int arg) {
-    int counterThread = 0;
-    if (arg < 2) {
-        //pthread_mutex_lock(&gMtx);
-        counterThread += arg;
-        //pthread_mutex_unlock(&gMtx);
-    } else {
-        compute(arg - 1);
-        compute(arg - 2);
+    if (arg < 2) return arg;
+    else return compute(arg - 1) + compute(arg - 2);
+    
+    // Fibonacci iterativo
+    /*int n1 = 0;
+    int n2 = 1;
+    for (int i = 0; i < arg; i++) {
+    	int n3 = n1 + n2;
+    	n1 = n2;
+    	n2 = n3;
     }
-    return counterThread;
+    return n1;*/
 }
 
 // Função wrapper que pode ser usada com pthread_create() para criar uma 
 // thread que retorna o resultado de compute(arg
 void* compute_thread(void* arg) {
+    // gValue era atribuida com zero nesta função ao entrar dentro do lock, não havendo efetivamente um compartilhamento de dados durante seu uso e fora desta função. Logo, sua existência é desnecessária.
+    int value = 0;
     int* ret = malloc(sizeof(int));
-    //pthread_mutex_lock(&gMtx);
-    //gValue = 0;
-    *ret =compute(*((int*)arg));
-    //pthread_mutex_unlock(&gMtx);
+    value += compute(*((int*)arg));
+    *ret = value;
     return ret;
 }
 
@@ -48,14 +47,6 @@ int main(int argc, char** argv) {
         return 1;
     }
 
-    //pthread_mutexattr_t Jatrb;
-    //pthread_mutexattr_init(&Jatrb);
-//
-    //pthread_mutexattr_settype(&Jatrb, PTHREAD_MUTEX_RECURSIVE);
-//
-    ////Inicializa o mutex
-    //pthread_mutex_init(&gMtx, Jatrb);
-
     int args[n_threads];
     int* results[n_threads];
     pthread_t threads[n_threads];
@@ -68,16 +59,13 @@ int main(int argc, char** argv) {
     for (int i = 0; i < n_threads; ++i)
         pthread_join(threads[i], (void**)&results[i]);
 
-    // Não usaremos mais o mutex
-    pthread_mutex_destroy(&gMtx);
-
     // Imprime resultados na tela
     // Importante: deve ser chamada para que a correção funcione
     imprimir_resultados(n_threads, results);
 
     // Faz o free para os resultados criados nas threads
-    for (int i = 0; i < n_threads; ++i) 
+    for (int i = 0; i < n_threads; ++i)
         free(results[i]);
-    
+
     return 0;
 }
